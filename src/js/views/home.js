123a532f65ddf35ff2c255d3b5efb76f2b1ba8f6
rafalebre <rafaellebre100@gmail.com>
2023-03-20 18:09:01 +0000

    Link created in home passing "my" data

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
@@ -7,6 +8,7 @@ export const Home = () => (
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
+			<Link to="/my/rafael"> Click Me </Link>
 		</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
